4555bdb61c9297e61e030311602ce265579365d3
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-22 15:57:48 -0300

    Getting data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {useEffect} from 'react'
 import 'bulma/css/bulma.min.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3000/speeds')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <div>
       <h1>Network Radar</h1>
