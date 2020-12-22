b5b7417e614590ef4171b4d471fc5f82ef489974
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-22 16:08:27 -0300

    class => className

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
           <h1>Network Radar</h1>
         </a>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
