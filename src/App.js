a1974b84c79bbb431e240fbc3a23fcc28d12cc74
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-22 16:02:39 -0300

    New component: Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {useEffect} from 'react'
 import 'bulma/css/bulma.min.css';
 
+import Header from './components/Header'
+
 function App() {
   useEffect(() => {
     fetch('http://localhost:3000/speeds')
@@ -10,6 +12,7 @@ function App() {
 
   return (
     <div>
+      <Header />
       <h1>Network Radar</h1>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <h1>Network Radar</h1>
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
